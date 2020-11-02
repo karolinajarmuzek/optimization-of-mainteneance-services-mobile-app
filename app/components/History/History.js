0f34b64a176913ece24b323e11d86ab729301295
karolinajarmuzek <karolina.jarmuzek@gmail.com>
2020-11-02 20:14:51 +0100

    datepicker changes

--- app/components/History/History.js
+++ app/components/History/History.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect, useCallback } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Platform } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 import { Icon } from "react-native-elements";
-import DatePicker from "react-native-datepicker";
+import DateTimePicker from "@react-native-community/datetimepicker";
 import { UIActivityIndicator } from "react-native-indicators";
 
 import { Container } from "../Container";
@@ -12,26 +12,22 @@ import styles from "./styles";
 import { ProfileIcon } from "../ProfileIcon";
 
 import { URL_REPAIR_BYTOKEN } from "../../urls";
+import { value } from "react-native-extended-stylesheet";
 
 function History({}) {
   const [data, setData] = useState([]);
+  const [showDatePicker, setShowDatePicker] = useState(false);
   const [dataFetched, setDataFetched] = useState(false);
   const token = useSelector((state) => state.user.token);
 
   var tempDate = new Date();
-  const [date, setDate] = useState(
-    String(("0" + tempDate.getDate()).slice(-2)) +
-      "-" +
-      String(("0" + (tempDate.getMonth() + 1)).slice(-2)) +
-      "-" +
-      tempDate.getFullYear()
-  );
+  const [date, setDate] = useState(new Date());
   const dispatch = useDispatch();
 
   function fetchData() {
     console.debug("Data fetch started");
     fetch(
-      URL_REPAIR_BYTOKEN + "/status=FINISHED/date=" + date.replace(/-/g, "."),
+      URL_REPAIR_BYTOKEN + "/status=FINISHED/date=" + formatDate(date, "."),
       {
         method: "GET",
         headers: {
@@ -46,8 +42,29 @@ function History({}) {
         console.debug("Data fetch completed successfully");
         setData(json);
         setDataFetched(true);
+      })
+      .catch((error) => {
+        console.debug("Error during fetching finished repairs: " + error);
       });
   }
+
+  function formatDate(dateToFormat, separator) {
+    var tempDate = new Date(dateToFormat);
+    var dateString =
+      String(("0" + tempDate.getDate()).slice(-2)) +
+      separator +
+      String(("0" + (tempDate.getMonth() + 1)).slice(-2)) +
+      separator +
+      tempDate.getFullYear();
+    return dateString;
+  }
+
+  const onChange = (event, selectedDate) => {
+    setShowDatePicker(Platform.OS === "ios");
+    const currentDate = selectedDate || date;
+    if (currentDate != date) setDataFetched(false);
+    setDate(currentDate);
+  };
 
   useEffect(() => {
     fetchData();
@@ -65,40 +82,25 @@ function History({}) {
           <Text style={styles.text}> Check your old tasks </Text>
         </View>
         <View style={styles.date}>
-          <DatePicker
-            style={{ width: 140 }}
-            date={date}
-            mode='date'
-            placeholder='select date'
-            format='DD-MM-YYYY'
-            minDate='2019-01-01'
-            maxDate={tempDate}
-            confirmBtnText='Confirm'
-            cancelBtnText='Cancel'
-            customStyles={{
-              dateText: {
-                color: "#023A5A",
-                fontSize: 20,
-                fontWeight: "bold",
-              },
-              dateInput: {
-                borderWidth: 0,
-                borderColor: "#023A5A",
-              },
-            }}
-            onDateChange={(date) => {
-              setDate(date);
-              dataFetched(false);
-            }}
-            iconComponent={
-              <Icon
-                name='calendar'
-                size={23}
-                type='font-awesome'
-                color='#023A5A'
-              />
-            }
+          <Text style={styles.dateText}> {formatDate(date, "-")} </Text>
+          <Icon
+            name='calendar'
+            size={23}
+            type='font-awesome'
+            color='#023A5A'
+            onPress={() => setShowDatePicker(true)}
           />
+          {showDatePicker ? (
+            <DateTimePicker
+              value={date}
+              mode='date'
+              placeholder='select date'
+              dateFormat='dayofweek day month'
+              minimumDate={new Date(2019, 0, 1)}
+              maximumDate={tempDate}
+              onChange={onChange}
+            />
+          ) : null}
         </View>
         {!dataFetched ? (
           <View style={styles.centerContent}>
--- app/components/History/styles.js
+++ app/components/History/styles.js
@@ -19,6 +19,13 @@ const styles = EStyleSheet.create({
     width: "100%",
     alignItems: "center",
     marginBottom: 30,
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  dateText: {
+    fontSize: 20,
+    color: "#023A5A",
+    fontWeight: "bold",
   },
   centerContent: {
     alignItems: "center",
